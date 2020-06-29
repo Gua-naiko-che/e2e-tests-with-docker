fd038b1301cf840d152aa97d31c1b48660a38f7e
Tao Gómez Gil <tao.gomez.gil@gmail.com>
2020-06-29 13:15:43 +0200

    Add Selenium test

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,13 +1,23 @@
-using System;
-using Xunit;
-
 namespace tests
 {
+    using System.IO;
+    using System.Reflection;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Xunit;
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            string driverDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            using (var driver = new ChromeDriver(driverDirectory))
+            {
+                driver.Navigate().GoToUrl("https://en.wikipedia.org/wiki/Climate_apocalypse");
+                By titleSelector = By.CssSelector("#firstHeading");
+                IWebElement title = driver.FindElement(titleSelector);
+                Assert.Equal("Climate apocalypse", title.Text);
+            }
 
         }
     }
