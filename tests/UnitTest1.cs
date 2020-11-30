e8df1096d63782511b4571403d38144a227f669a
Tao Gómez Gil <tao.gomez.gil@gmail.com>
2020-11-30 14:20:39 +0100

    Use env variable to switch between DEV and CI envs

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 namespace tests
 {
+    using System;
     using System.IO;
     using System.Reflection;
     using OpenQA.Selenium;
@@ -10,13 +11,19 @@ namespace tests
         [Fact]
         public void Test1()
         {
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "chromedriver");
+            bool inDocker = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
+            var driverFileName = inDocker ? "chromedriver" : "chromedriver.exe";
+            var appRootUrl = inDocker ? "http://webapp:80/" : "https://localhost:5001/";
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), driverFileName);
             var options = new ChromeOptions();
-            options.AddArgument("--headless");
-            options.AddArgument("--no-sandbox");
-            using var driver = new ChromeDriver(service, options);
+            if (inDocker)
+            {
+                options.AddArgument("--headless");
+                options.AddArgument("--no-sandbox");
+            }
 
-            driver.Navigate().GoToUrl("http://webapp:80");
+            using var driver = new ChromeDriver(service, options);
+            driver.Navigate().GoToUrl(appRootUrl);
             By titleSelector = By.CssSelector("h1");
             IWebElement title = driver.FindElement(titleSelector);
             Assert.Equal("Welcome", title.Text);
